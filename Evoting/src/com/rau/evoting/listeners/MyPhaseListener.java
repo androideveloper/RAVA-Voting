799ef50e86833cc87ca32af529ce36f93c12d325
Roland <roland.yeghiazaryan@gmail.com>
2013-03-12 17:17:56 +0400

    ctrl+shift+f

--- Evoting/src/com/rau/evoting/listeners/MyPhaseListener.java
+++ Evoting/src/com/rau/evoting/listeners/MyPhaseListener.java
@@ -5,18 +5,18 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
-public class MyPhaseListener implements PhaseListener  {
+public class MyPhaseListener implements PhaseListener {
 
 	@Override
 	public void afterPhase(PhaseEvent arg0) {
 		FacesContext context = FacesContext.getCurrentInstance();
-		
+
 	}
 
 	@Override
 	public void beforePhase(PhaseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
