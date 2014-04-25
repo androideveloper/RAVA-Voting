4b1c6ff6f874c3ed4c3b987aee83d8e4e27e6df8
Aram Gevorgyan <aramgv@gmail.com>
2014-04-25 18:15:47 +0400

    fixes

--- Evoting/src/com/rau/evoting/beans/Home.java
+++ Evoting/src/com/rau/evoting/beans/Home.java
@@ -17,68 +17,43 @@ import com.restfb.types.User;
 
 /**
  * @author Aram
- *
+ * 
  */
 public class Home {
-	/*private String accessToken;
-	
+
 	public Home() {
 	}
-	
-	public String getAccessToken() {
-		return accessToken;
-	}
 
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
-
-	@PostConstruct
-	public void init() {
-		System.out.println("home init");
-		if(!FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("accessToken")) {
-			HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-			String code = req.getParameter("code");
-			accessToken = FacebookService.getInstance().getAccessToken(code, "Elections.xhtml");
-			FacebookClient fbClient = new DefaultFacebookClient(accessToken);
-			User user = fbClient.fetchObject("me", User.class);
-			int userId = SqlDataProvider.getInstance().insertUser(user.getId(), user.getEmail());
-			Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-			sessionMap.put("accessToken", accessToken); 
-			Connection<Group> gr = fbClient.fetchConnection("me/groups", Group.class);
-			List<Group> groups = gr.getData();
-			sessionMap.put("userGroups", groups);  
-			SqlDataProvider.getInstance().insertUserGroups(userId, groups); 
-			sessionMap.put("userId", userId);
-		}
-		// load groups not depending on if
-	}*/
-	
 	public void getAccessToken() {
 		System.out.println("home init");
-		if(!FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("accessToken")) {
-			HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		if (!FacesContext.getCurrentInstance().getExternalContext()
+				.getSessionMap().containsKey("accessToken")) {
+			HttpServletRequest req = (HttpServletRequest) FacesContext
+					.getCurrentInstance().getExternalContext().getRequest();
 			String code = req.getParameter("code");
-			String accessToken = FacebookService.getInstance().getAccessToken(code, "Home.xhtml");
+			String accessToken = FacebookService.getInstance().getAccessToken(
+					code, "Home.xhtml");
 			FacebookClient fbClient = new DefaultFacebookClient(accessToken);
-			User user = fbClient.fetchObject("me", User.class);
+			User user = fbClient.fetchObject("me", User.class); 
 			int userId = UserDP.insert(user.getId(), user.getEmail());
-			Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-			sessionMap.put("accessToken", accessToken); 
-			Connection<Group> gr = fbClient.fetchConnection("me/groups", Group.class);
+			Map<String, Object> sessionMap = FacesContext.getCurrentInstance()
+					.getExternalContext().getSessionMap();
+			sessionMap.put("accessToken", accessToken);
+			Connection<Group> gr = fbClient.fetchConnection("me/groups",
+					Group.class);
 			List<Group> groups = gr.getData();
-			sessionMap.put("userGroups", groups);  
-			UserGroupDP.insert(userId, groups); 
+			sessionMap.put("userGroups", groups);
+			UserGroupDP.insert(userId, groups);
 			sessionMap.put("userId", userId);
 			sessionMap.put("username", user.getName());
 		}
 		return;
 	}
-	
+
 	public String getUsername() {
 		String username = (String) FacesContext.getCurrentInstance()
 				.getExternalContext().getSessionMap().get("username");
 		return username;
 	}
-			
+
 }
