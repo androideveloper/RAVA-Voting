f8cf6f0003b752dca3b721617ed7a9c2ba85cd42
Roland <roland.yeghiazaryan@gmail.com>
2014-04-30 17:06:02 +0400

    Android project

---
+++ RavaVotingAndroid/src/com/rava/voting/ui/MainActivity.java
@@ -0,0 +1,176 @@
+package com.rava.voting.ui;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rava.voting.R;
+
+public class MainActivity extends Activity implements
+		NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+	/**
+	 * Fragment managing the behaviors, interactions and presentation of the
+	 * navigation drawer.
+	 */
+	private NavigationDrawerFragment mNavigationDrawerFragment;
+
+	/**
+	 * Used to store the last screen title. For use in
+	 * {@link #restoreActionBar()}.
+	 */
+	private CharSequence mTitle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
+
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
+
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		onSectionAttached(position);
+		FragmentManager fragmentManager = getFragmentManager();
+
+		switch (position) {
+		case 0:
+			Fragment fragment = fragmentManager
+					.findFragmentById(R.id.container);
+			if (fragment != null && fragment instanceof ScannerFragment) {
+				return;
+			} else {
+				fragmentManager
+						.beginTransaction()
+						.replace(R.id.container, ScannerFragment.newInstance(),
+								ScannerFragment.TAG).commit();
+			}
+			break;
+
+		case 1:
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container,
+							PlaceholderFragment.newInstance(position)).commit();
+			break;
+
+		case 2:
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container,
+							PlaceholderFragment.newInstance(position)).commit();
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	public void onSectionAttached(int number) {
+		switch (number) {
+		case 0:
+			mTitle = getString(R.string.section_scan_receipt);
+			break;
+		case 1:
+			mTitle = getString(R.string.section_my_elections);
+			break;
+		case 2:
+			mTitle = getString(R.string.section_vote);
+			break;
+		}
+	}
+
+	public void restoreActionBar() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(mTitle);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			// Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.main, menu);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		private static final String ARG_SECTION_NUMBER = "section_number";
+
+		/**
+		 * Returns a new instance of this fragment for the given section number.
+		 */
+		public static PlaceholderFragment newInstance(int sectionNumber) {
+			PlaceholderFragment fragment = new PlaceholderFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment, container,
+					false);
+			TextView textView = (TextView) rootView
+					.findViewById(R.id.section_label);
+			textView.setText(Integer.toString(getArguments().getInt(
+					ARG_SECTION_NUMBER)));
+			return rootView;
+		}
+
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((MainActivity) activity).onSectionAttached(getArguments().getInt(
+					ARG_SECTION_NUMBER));
+		}
+	}
+
+}
---
+++ RavaVotingAndroid/src/com/rava/voting/ui/ScannerFragment.java
@@ -0,0 +1,82 @@
+package com.rava.voting.ui;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.rava.voting.R;
+
+public class ScannerFragment extends Fragment {
+
+	public static final String TAG = "ScannerFragment";
+
+	private Button mButtonScan;
+	private TextView mTextViewFormat;
+	private TextView mTextViewContent;
+
+	public static ScannerFragment newInstance() {
+		ScannerFragment fragment = new ScannerFragment();
+		Bundle args = new Bundle();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public ScannerFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.fragment_scanner, container,
+				false);
+		mButtonScan = (Button) root.findViewById(R.id.button_scan);
+		mTextViewFormat = (TextView) root.findViewById(R.id.scan_format);
+		mTextViewContent = (TextView) root.findViewById(R.id.scan_content);
+
+		mButtonScan.setOnClickListener(new ScanClick());
+
+		return root;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(
+				requestCode, resultCode, data);
+		if (scanningResult != null) {
+			String format = scanningResult.getFormatName();
+			String content = scanningResult.getContents();
+			mTextViewFormat.setText("FORMAT: " + format);
+			mTextViewContent.setText("CONTENT: " + content);
+		} else {
+			Toast.makeText(getActivity(), "No result", Toast.LENGTH_SHORT)
+					.show();
+			mTextViewFormat.setText("No result");
+			mTextViewContent.setText("No result");
+		}
+	}
+
+	private class ScanClick implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			IntentIntegrator integrator = new IntentIntegrator(
+					ScannerFragment.this);
+			integrator.addExtra("SCAN_WIDTH", 500);
+			integrator.addExtra("SCAN_HEIGHT", 500);
+			integrator
+					.addExtra("PROMPT_MESSAGE",
+							"Place a receipt barcode inside the viewfinder square to scan it");
+			integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+		}
+
+	}
+
+}
