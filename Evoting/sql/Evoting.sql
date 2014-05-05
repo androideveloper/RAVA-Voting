15cc000f316616d0f16871e1a1507e8855e2fa00
vahan8 <vahangev8@gmail.com>
2014-05-05 20:42:20 +0400

    unicode fix

--- Evoting/sql/Evoting.sql
+++ Evoting/sql/Evoting.sql
@@ -49,8 +49,8 @@ create table ElectionVotes
 	userId int foreign key references Users(id),
 	auditBallot tinyint,
 	auditSequence varchar(256),
-	encoded1 varchar(1024),
-	encoded2 varchar(1024),
+	encoded1 nvarchar(1024),
+	encoded2 nvarchar(1024),
 	answerId int,
 	--chaumPedersen1 varchar(1024),
 	--chaumPedersen2 varchar(1024)
